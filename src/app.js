996a424b1e9af57237af9984ee8bdfca774302c4
Agustin Martinez <agustinlevimb@gmail.com>
2024-05-19 22:51:09 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const suspects = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const actions = ['ate', 'peed', 'crushed', 'broke'];
+const items = ['my homework', 'my phone', 'the car'];
+const timings = ['before the class', 'while I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const getRandomItem = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  return array[randomIndex];
 };
+
+const getRandomExcuse = () => {
+  const culprit = getRandomItem(suspects);
+  const action = getRandomItem(actions);
+  const item = getRandomItem(items);
+  const timing = getRandomItem(timings);
+
+  return `${culprit} ${action} ${item} ${timing}`;
+};
+
+const excuseParagraph = document.getElementById('excuse');
+
+excuseParagraph.textContent = getRandomExcuse();
+
+
